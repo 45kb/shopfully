ce08b979196588451b4adb67cb5929ead0869980
45kb <filippo.oretti@gmail.com>
2018-07-18 17:45:01 +0200

    removed jquery

---
+++ src/js/favourites.js
@@ -0,0 +1,32 @@
+'use strict'
+//Favaourites
+//init database if not present
+if (!localStorage.getItem('favourites')) {
+  localStorage.setItem('favourites', JSON.stringify([]))
+}
+
+var favourites = JSON.parse(localStorage.getItem('favourites'))
+, saveFavourite = function (identifier) {
+  var id = identifier.toString();
+  //if not saved save it
+  if (favourites.indexOf(id) < 0) {
+    favourites.push(id);
+    localStorage.setItem('favourites', JSON.stringify(favourites));
+    
+  } else {
+    deleteFavourite(identifier);
+  }
+
+  console.info('Saved in favourites', favourites);
+}
+, deleteFavourite = function (identifier) {
+
+  var id = identifier.toString()
+  , indexOf = favourites.indexOf(id);
+
+  if (indexOf > -1) {
+    favourites.splice(indexOf, 1);
+  }
+  localStorage.setItem('favourites', JSON.stringify(favourites));
+  console.info('Deleted in favourites', favourites);
+};
---
+++ src/js/layout.js
@@ -0,0 +1,17 @@
+'use strict'
+//Layout
+var sideOverlay = document.getElementById('side-overlay')
+  , sideMenuClosebutton = document.getElementById('side-menu-close-button')
+  , navbarButton = document.getElementById('navbar-button')
+  , toggleSidemenu = function () {
+    //toggle mobile side menu
+    navbarButton.onclick = function () {
+      sideOverlay.classList.toggle('show');
+    };
+
+    sideMenuClosebutton.onclick =  function () {
+      sideOverlay.classList.remove('show');
+    };
+  };
+
+  toggleSidemenu();
